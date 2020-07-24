ed769bba2c6ebe45df5b5b857e0e849549af2f97
Deepak <deepakkumarjha@outlook.com>
2020-07-25 02:45:54 +0530

    added dynamic chart with actual data set

--- src/components/graph.js
+++ src/components/graph.js
@@ -1,30 +1,31 @@
 import React from "react";
-import LineChart from "react-linechart";
-import "../../node_modules/react-linechart/dist/styles.css";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { Line } from "react-chartjs-2";
 
-const Graph = () => {
-  const data = [
-    {
-      color: "steelblue",
-      points: [
-        { x: 10, y: 20 },
-        { x: 20, y: 40 },
-        { x: 30, y: 10 },
-        { x: 35, y: 54 },
-        { x: 70, y: -30 },
-      ],
-    },
-  ];
+const Graph = (props) => {
+  const { news } = props;
+  let currentVotes = JSON.parse(localStorage.getItem("currentVotes")) || {};
+  let graphLabels = [];
+  let graphDataSet = [];
+  if (news[0]) {
+    news[0].hits.forEach((item) => {
+      graphDataSet.push(currentVotes[item.objectID] || 0);
+      graphLabels.push(item.objectID);
+    });
+  }
+  console.log(graphDataSet);
+  const data = {
+    labels: graphLabels,
+    datasets: [{ label: "votes", data: graphDataSet }],
+  };
   return (
     <div className="graph-container">
-      <LineChart
-        width={400}
-        height={200}
-        xLabel="ID"
-        yLabel="Votes"
-        data={data}
-      />
+      <Line width={400} height={200} xLabel="ID" yLabel="Votes" data={data} />
     </div>
   );
 };
-export default Graph;
+function mapStateToProps({ news }) {
+  return { news };
+}
+export default withRouter(connect(mapStateToProps)(Graph));
