a334986525342f6ed55ad622bd2281046a9b21a9
Deepak <deepakkumarjha@outlook.com>
2020-07-25 01:46:05 +0530

    implemented hide button

--- src/components/newsitem.js
+++ src/components/newsitem.js
@@ -5,9 +5,8 @@ import { connect } from "react-redux";
 import upVoteImg from "../assets/up.png";
 const NewsItem = (props) => {
   let listItems = <div></div>;
-  let currentVotes = {};
-  currentVotes = JSON.parse(localStorage.getItem("currentVotes")) || {};
-
+  let currentVotes = JSON.parse(localStorage.getItem("currentVotes")) || {};
+  let currentHides = JSON.parse(localStorage.getItem("currentHides")) || {};
   const getDomain = (url) => {
     if (url) {
       const url_object = new URL(url);
@@ -21,32 +20,48 @@ const NewsItem = (props) => {
     currentVotes[objectID] = vote + 1;
     localStorage.setItem("currentVotes", JSON.stringify(currentVotes));
   };
+  const hide = (objectID) => {
+    currentHides[objectID] = true;
+    localStorage.setItem("currentHides", JSON.stringify(currentHides));
+  };
   if (props.news && props.news.length > 0) {
     const newsFeedData = props.news[0].hits;
-    listItems = newsFeedData.map((newsFeed) => (
-      <div key={newsFeed.objectID} className="d-flex gray-background">
-        <div className="comments-section">{newsFeed.num_comments} </div>
-        <div className="vote-count-section">
-          {currentVotes[newsFeed.objectID] || 0}
-        </div>
-        <div className="up-vote-section">
-          {" "}
-          <a href="#" onClick={() => upVote(newsFeed.objectID)}>
-            <img src={upVoteImg} width="20%" />
-          </a>{" "}
-        </div>
-        <div className="font-weight-bold news-section">
-          {newsFeed.title}{" "}
-          <span className="small-font">
-            <a href={newsFeed.url} target="_blank">
-              ({getDomain(newsFeed.url)})
+    listItems = newsFeedData.map((newsFeed) =>
+      currentHides[newsFeed.objectID] ? (
+        ""
+      ) : (
+        <div key={newsFeed.objectID} className="d-flex gray-background">
+          <div className="comments-section">{newsFeed.num_comments} </div>
+          <div className="vote-count-section">
+            {currentVotes[newsFeed.objectID] || 0}
+          </div>
+          <div className="up-vote-section">
+            {" "}
+            <a href="#" onClick={() => upVote(newsFeed.objectID)}>
+              <img src={upVoteImg} width="20%" />
             </a>{" "}
-            by <span className="text-black">{newsFeed.author}</span>{" "}
-            <TimeAgo date={newsFeed.created_at} /> <a href="">[ hide ]</a>
-          </span>{" "}
+          </div>
+          <div className="font-weight-bold news-section">
+            {newsFeed.title}{" "}
+            <span className="small-font">
+              <a href={newsFeed.url} target="_blank">
+                ({getDomain(newsFeed.url)})
+              </a>{" "}
+              by <span className="text-black">{newsFeed.author}</span>{" "}
+              <TimeAgo date={newsFeed.created_at} />{" "}
+              <a
+                href=""
+                onClick={(e) => {
+                  hide(newsFeed.objectID);
+                }}
+              >
+                [ hide ]
+              </a>
+            </span>{" "}
+          </div>
         </div>
-      </div>
-    ));
+      )
+    );
   }
   return (
     <div>
